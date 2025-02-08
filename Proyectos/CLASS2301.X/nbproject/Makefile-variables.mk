f313ec265dacbabfd7476757e15c0becde817e7a
Juan Delgado <79658379+Juan-delgado1@users.noreply.github.com>
2025-02-08 16:09:30 -0500

    Add files via upload

---
+++ Proyectos/CLASS2301.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,13 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=CLASS2301.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/CLASS2301.X.production.hex
+CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
+CND_PACKAGE_NAME_default=class2301.x.tar
+CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/class2301.x.tar
