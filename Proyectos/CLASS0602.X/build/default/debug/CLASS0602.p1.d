d1c8c3b302047b420ecebf947c8b7796b1c0d983
Juan Delgado <79658379+Juan-delgado1@users.noreply.github.com>
2025-02-08 16:03:34 -0500

    Add files via upload

---
+++ Proyectos/CLASS0602.X/build/default/debug/CLASS0602.p1.d
@@ -0,0 +1,3 @@
+ build/default/debug/CLASS0602.d  \
+ build/default/debug/CLASS0602.p1:  \
+ CLASS0602.c 
---
+++ Proyectos/CLASS0602.X/build/default/production/CLASS0602.p1.d
@@ -0,0 +1,3 @@
+ build/default/production/CLASS0602.d  \
+ build/default/production/CLASS0602.p1:  \
+ CLASS0602.c 
